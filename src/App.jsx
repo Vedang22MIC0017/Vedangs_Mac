31183ecf903d9cd91b169fce05809c9fe1ff4667
Vedang Mishrra <145650247+justvedang@users.noreply.github.com>
2026-02-15 01:06:16 +0530

    added the higher order component for draggable windows implemented for terminals

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Navbar from "#components/Navbar.jsx";
 import Welcome from '#components/Welcome';
 import Dock from '#components/Dock';
+import { Draggable } from 'gsap/all';
+import gsap from 'gsap';
+import { Terminal } from '#windows';
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Welcome/>
       <Dock/>
+      <Terminal/>
     </main>
   );
 };
